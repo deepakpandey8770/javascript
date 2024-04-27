08bd607266db68b8be9c0114b1156ef72d862e1f
deepakpandey8770 <155836984+deepakpandey8770@users.noreply.github.com>
2024-04-27 12:53:57 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=12345
+let accountEmail="depak@gmail.com"
+var accountPass="12345"
+accountCity='bhopal'
+
+
+// accountId =21546
+console.log(accountId);
+accountEmail="hc@hhs.comṇ"
+accountPass='124565'
+accountCity='indore'
+
+console.table([accountId,accountEmail,accountPass,accountCity])
