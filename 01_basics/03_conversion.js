b781ac99e2664c97ee71a7b047d29734c9a5d4ed
deepakpandey8770 <155836984+deepakpandey8770@users.noreply.github.com>
2024-04-28 13:55:54 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,30 @@
+let score=null//"33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33">>33
+//"33abc">>Nan
+//true>> 1; false>>0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1>>true; 0>>false
+//"">>false
+//"deepak">>true
+
+
+let Number=55
+
+let stringNumber=String(Number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
